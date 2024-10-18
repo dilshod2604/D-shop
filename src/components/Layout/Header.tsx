4f08b639c7098ad6cd0c32ca0649f35fd85c3475
unknown <dilshodadilbekov456@gmail.com>
2024-10-18 22:29:50 +0600

    Navbar is finished

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -7,8 +7,8 @@ const Header = () => {
       <div className="container">
         <div className="flex items-start justify-between pt-5 pb-2 border-b border-neutral-400">
           <div className="flex items-center justify-center ">exclisive</div>
+          <Navbar />
         </div>
-        <Navbar />
       </div>
     </header>
   );
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,13 +1,30 @@
+"use client";
 import { links } from "@/constants/links";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+  const pathName = usePathname();
   return (
     <ul className="flex items-center  gap-x-[48px] text-black">
       {links.map((link, index) => (
         <li key={index}>
-          <Link href={link.href}>{link.name}</Link>
+          {pathName === link.href ? (
+            <Link
+              href={link.href}
+              className="text-black font-bold pb-2 underline transition"
+            >
+              {link.name}
+            </Link>
+          ) : (
+            <Link
+              href={link.href}
+              className="text-neutral-600 font-normal transition   "
+            > 
+              {link.name}
+            </Link>
+          )}
         </li>
       ))}
     </ul>
