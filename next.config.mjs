c5293d7a54a230627174714aed0b7918cfb7eff0
unknown <dilshodadilbekov456@gmail.com>
2024-11-03 01:27:10 +0600

    fixed errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, 
-    swcMinify: true,       
-    images: {
-      domains: ['example.com'], 
-    },
-    env: {
-      CUSTOM_VARIABLE: process.env.CUSTOM_VARIABLE, 
-    },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.mp4$/,      
-        type: 'asset/resource'
-      });
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
