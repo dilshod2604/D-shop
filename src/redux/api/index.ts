4d866636445e46fe48b080a89ff0a852c5bba44c
unknown <dilshodadilbekov456@gmail.com>
2024-11-03 09:54:27 +0600

    front-end url is  changed

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -5,8 +5,7 @@ import {
 } from "@reduxjs/toolkit/query/react";
 
 const BaseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000/"
-  // baseUrl: "https://d-shop-dilshod2604s-projects.vercel.app/",
+  baseUrl: "https://d-shop-kappa.vercel.app/",
 });
 
 const baseQueryExtends: BaseQueryFn = async (arg, api, extraOptions) => {
@@ -19,6 +18,6 @@ export const api = createApi({
   baseQuery: baseQueryExtends,
   refetchOnFocus: true,
   refetchOnReconnect: true,
-  tagTypes: ["product", "auth","cart","search","favorite"],
+  tagTypes: ["product", "auth", "cart", "search", "favorite"],
   endpoints: () => ({}),
 });
